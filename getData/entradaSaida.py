aad7bfcb3d150f4d82924377896272481526fc30
flp-gregorio <felipealvesgregorio01@gmail.com>
2023-03-29 01:19:43 -0300

    update

--- getData/entradaSaida.py
+++ getData/entradaSaida.py
@@ -8,11 +8,11 @@ def criaMatriz(instancia):
     return data
 
 def saidaResultado(instancia, matriz):
-    print('\n' 'NOME DA INST NCIA:', str(instancia)) # Imprime o nome da Instancia 
+    print('\n' 'NOME DA INSTANCIA:', str(instancia)) # Imprime o nome da Instancia 
     linhas = matriz.shape[0] # Pega o primeiro elemento do array matriz que corresponde ao número de linhas
     colunas = matriz.shape[1] # Pega o segundo elemento do array matriz que corresponde ao número de colunas
     print("A matriz tem dimensões: " + str(linhas) + "x" + str(colunas) + '\n')
-    path = 'C:/Users/Felip/Documents/grafosmeu/resultados.txt' # Define o endereço de documento que armazenará os resultados
-    arquivo = open(path, "a+") # Abre o arquivo no diretório definido pela  variável path
+    path = 'C:/Users/Felip/Documents/grafosmeu/Resultados/' # Define o endereço de documento que armazenará os resultados
+    arquivo = open(path + instancia +  '_dimensao', "a+") # Abre o arquivo no diretório definido pela  variável path
     arquivo.writelines(str(linhas) + ' ' + str(colunas) + '\n') # Escreve no documento resultados.txt o número de linhas e colunas da matriz lida
     arquivo.close # Fecha o arquivo
